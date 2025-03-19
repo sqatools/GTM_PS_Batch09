c859feae09ae12040f8b67528fcff0dfcabab677
atafahmad1996 <atafahmad1996@gmail.com>
2025-03-19 21:21:04 +0530

    adding python variable file

---
+++ AtafPerwez/PythonProgramming/Python_Variables/python_variable.py
@@ -0,0 +1,16 @@
+# Python variable
+
+x = 50
+Y=100
+
+print(id(x))
+
+
+##########################
+# assigning multiple variable at a time
+
+a, b, c = 10, 20, 30
+print(a, b, c)
+print('value of a:', a)
+print('value of b:', b)
+print('value of c:', c)
