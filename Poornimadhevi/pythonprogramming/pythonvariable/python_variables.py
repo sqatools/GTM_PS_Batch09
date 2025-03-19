3a27d1a31e5ba694eb76ce5724200d4827dc10b4
poornimadevirengaraja <r.poornimadevi29@gmail.com>
2025-03-19 19:24:23 +0400

    python variables

--- Poornimadhevi/pythonprogramming/pythonvariable/python_variables.py
+++ Poornimadhevi/pythonprogramming/pythonvariable/python_variables.py
@@ -5,3 +5,15 @@ a = 10
 
 # address of the variable
 print (id(a))
+
+########################################
+x = 50
+y = 50
+print("address of x:",x,id(x))
+print("address of y:",y,id(y))
+########################################
+x=y=z =100
+print("value X",x,id(x))
+print("value y",y,id(y))
+print("value z",z,id(z))
+################################
