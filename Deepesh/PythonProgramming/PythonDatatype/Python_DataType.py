9f141e7b09a154d2a384706a8432bd8f243c4741
Bi12Python <xovek11875@bitvoo.com>
2025-03-20 21:02:03 +0530

    Python data type session1

---
+++ Deepesh/PythonProgramming/PythonDatatype/Python_DataType.py
@@ -0,0 +1,55 @@
+"""
+Python Data Types :
+
+1.  Numbers
+    i) Integer
+    ii) Float
+    iii) Complex Number
+
+2.  Sequential
+    i)   String
+    ii)  List
+    iii) Tuple
+
+
+3.  Dictionary
+4.  Set
+5.  Boolean
+"""
+
+############ Integer #################
+"""
+# properties of integer
+->  Integer is immutable data type, once it is defined we can not change it.
+->  Integer can contains any position and negative value.
+->  Only whole will be consider as int
+->  There is no limit to defined data in int variable
+
+"""
+n1 = 0
+n2 = 4
+n3 = 100
+n4 = 45435435434354545454222333333333
+n5 = -5644545
+
+print(n1, type(n1)) # 0 <class 'int'>
+print(n2, type(n2)) # 4 <class 'int'>
+print(n3, type(n3)) # 100 <class 'int'>
+print(n4, type(n4)) # 45435435434354545454222333333333 <class 'int'>
+print(n5, type(n5)) # -5644545 <class 'int'>
+
+
+
+
+# when we multiply string with number, then it repeats the string that number of times.
+var1 = 'Hello'
+print(var1*5)
+
+print("_"*50)
+############ Float #################
+"""
+->  Float is immutable data type.
+->  Float only contains pointer values. e.g 4.5, 5.6, 0.23
+->  Any positive and negative values can be float data type
+->  There is no limit for float data type
+"""
