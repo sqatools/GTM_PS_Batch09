7526483ac3ead7759953ad6db95fde61463f3486
martferns <martha.fernandes@proton.me>
2025-03-20 17:45:49 +0000

    Python datatype session notes

--- MarthaFernandes/PythonProgramming/PythonDataType/Python_DataType.py
+++ MarthaFernandes/PythonProgramming/PythonDataType/Python_DataType.py
@@ -1,4 +1,5 @@
-""Python Data TypeError
+"""
+Python Data TypeError
 
 1. Numbers
 i) Integer
@@ -14,15 +15,15 @@ III) Complex Number
 4. Set
 5. Boolean
 
-
+"""
 ############Integer ################
-*#
-# properties of Integer
+
+"""# properties of Integer
 #-> Integer is immutable data type, once it is defined we can not change it.
 # -> Integer can contains any position and negative value
 # -> Only whole will be consider as int
 # -> There is no limit to defined data in int variable
-""
+"""
 
 n1 = 0
 n2 = 4
@@ -30,11 +31,11 @@ n3 = 100
 n4 = 237463278426064246290
 n5 = -67655456
 
-print (n1,type(n1)) # 0 <class 'int'>
-print (n2, type(n2)_# 4 <class 'int>
-print (n3, type(n3))_# 100 <class 'int>'
-print (n4,type(n4))_# 237463278426064246290
-print (n5, type(n5))_# -67655456 <class 'int'>
+print(n1, type(n1)) # 0 <class 'int'>
+print(n2, type(n2)) # 4 <class 'int>
+print(n3, type(n3)) # 100 <class 'int>'
+print(n4,type(n4)) # 237463278426064246290
+print(n5, type(n5))# -67655456 <class 'int'>
 
 # when we multiply string with number , then it repeats the string that number of times
 var1 = 'Hello'
