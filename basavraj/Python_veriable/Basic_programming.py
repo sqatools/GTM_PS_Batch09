54ec249345b64a0d7f1dd236f05923c8dad13006
Megs228 <v.megha228@gmail.com>
2025-03-21 14:05:26 +0530

    added some basic programs

---
+++ basavraj/Python_veriable/Basic_programming.py
@@ -0,0 +1,72 @@
+
+
+# 1). Python Program to add two integer values.
+
+X,Y=20,50
+print(X+Y)
+
+#2). Python Program to subtract two integer values.
+
+X,Y=20,50
+print(Y-X)
+
+#3. Python program to multiply two numbers.
+
+a,b=2,6
+print(a*b)
+
+# 4). Python program to repeat a given string 5 times.
+Name="Basavaraj"
+print(Name*5)
+
+#5). Python program to get the Average of given numbers.
+
+x,y,z=10,20,30
+avg=(x+y+z)/3
+print(avg)
+
+
+#6). Python program to get the median of given numbers.
+
+#7). Python program to print the square and cube of a given number.
+
+a=3
+print(a**2)
+print(a**3)
+
+#8). Python program to interchange values between variables.
+
+a,b=10,20
+a,b=b,a
+print(a,b)
+
+#10). Python program to solve the given math formula.
+#Formula : (a + b)2 = a^2 + b^2 + 2ab
+
+a=2
+b=3
+c=(a+b)**2
+print(c)
+d=a**2+b**2+2*a*b
+print(d)
+print(c==d)
+
+#11). Python program to solve the given math formula.
+#Formula : (a – b)2 = a^2 + b^2 – 2ab
+a=2
+b=3
+c=(a-b)**2
+print(c)
+d=a**2+b**2-2*a*b
+print(d)
+print(c==d)
+
+#12). Python program to solve the given math formula.
+#Formula : a2 – b2 = (a-b)(a+b)
+
+a=3
+b=2
+c=(a-b)*(a+b)
+print(c)
+
+
