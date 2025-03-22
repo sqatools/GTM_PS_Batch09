92a31ed8b3f48204c7d4170b044c7c544378b708
Varinderbatth1 <varinderkbatth@gmail.com>
2025-03-22 13:47:24 -0400

    adding datatype file

---
+++ Varinder/python_programing/datatype_class.py
@@ -0,0 +1,23 @@
+############ Integer #################
+"""
+# properties of integer
+->  Integer is immutable data type, once it is defined we can not change it.
+->  Integer can contains any position and negative value.
+->  Only whole will be consider as int
+->  There is no limit to defined data in int variable
+
+"""
+int1 = 100
+int2 = 120
+int3 = 40
+int4 = int1 + int2 + int3
+print(int4, type(int2))
+
+############################################
+# Sting datatype
+name = "varinder"
+Add = "123 you got it"
+phone = "703-450-6043"
+
+print("this is my name : " ,name, type(name))
+
