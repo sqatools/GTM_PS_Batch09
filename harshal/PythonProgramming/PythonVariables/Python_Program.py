555a25d9ebb897fe64e9bbe4c7faab1871c0dbfb
HarshalB009 <harshal.borgude@gmail.com>
2025-03-26 14:26:37 +0530

    simple programe

---
+++ harshal/PythonProgramming/PythonVariables/Python_Program.py
@@ -0,0 +1,75 @@
+a1=10
+b1=20
+c1=a1+b1
+print("Addition of", a1, "and", b1 , "is", c1)
+
+print("_"*50)
+j="Harshal "
+print(j * 5)
+
+print("_"*50)
+
+#5). Python program to get the Average of given numbers.
+#Formula: sum of all the number/ total number
+#Input:
+#a = 40
+#b = 50
+#c = 30
+
+a = 40
+b = 50
+c = 30
+avg=(a + b + c)/3
+print(avg)
+
+print("_"*50)
+
+#7). Python program to print the square and cube of a given number.
+#Input :
+#num1 = 9
+
+num1=9
+sqr=num1**2
+cube=num1**3
+print("Squre of", num1, "is", sqr, "and cube is", cube)
+
+print("_"*50)
+
+#8). Python program to interchange values between variables.
+#Input :
+#a = 10
+#b = 20
+
+a = 10
+b = 20
+a , b = b , a
+print("Value of a is", a)
+print("Value of b is", b)
+
+print("_"*50)
+
+#9). Python program to solve this Pythagorous theorem.
+#Theorem : (a2 + b2 = c2)
+import math
+aa=3
+bb=4
+c=math.sqrt=(aa**2)+(bb**2)
+print("As per Pythagorous theoram" ,aa, "+", bb, "=", c)
+
+print("_"*50)
+
+#10). Python program to solve the given math formula.
+#Formula : (a + b)2 = a^2 + b^2 + 2ab
+a=2
+b=3
+c=(a**2 + b**2 + (2*a*b))
+print("(a + b)^2 =", c)
+
+print("_"*50)
+
+#12). Python program to solve the given math formula.
+#Formula : a2 – b2 = (a-b)(a+b)
+a=5
+b=6
+c=(a-b) * (a+b)
+print("a^2 - b^2 =",c)
---
+++ harshal/PythonProgramming/PythonVariables/Python_Variables.py
@@ -0,0 +1,19 @@
+print('Hello Harshal')
+a= 9
+b= 7
+c=a*b
+print("Multiplication is: ",c)
+
+
+abc= 10 + 20j
+print(abc)
+print(type(abc))
+
+print("_"*50)
+
+tup=(7 , 9 , "Harshal" , 11 , ("i", "Love", 3) , "Jaya" , 27)
+print(tup[2])
+
+dict={'Name':'Harshal Borgude',"Age":29,'Location':'Mumbai',"Contact No":9404686368}
+print(dict['Contact No'])
+print(dict['Location'])
