1ab2d0e6bb89cf91148951cf58c19cc39521598b
Varinderbatth1 <varinderkbatth@gmail.com>
2025-03-27 21:37:15 -0400

    conversion file

--- Deepesh/PythonProgramming/PythonDatatype/Datatype_Conversion.py
+++ Deepesh/PythonProgramming/PythonDatatype/Datatype_Conversion.py
@@ -410,3 +410,10 @@ print(l3, type(l3))
 ## bool -> tuple ###  conversion not possible
 ## bool -> dict ###  conversion not possible
 ## bool -> set ###  conversion not possible
+
+######convert list to tuple #######
+
+lis = [10, 20, 100, "varinder"]
+print(lis)
+tup = tuple(lis)
+print("my result would display in tuple:", tup, type(tup))
