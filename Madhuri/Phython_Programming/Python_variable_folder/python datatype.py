1c95f9ebaeedfd7e3a20cb0537b02a4a44556953
Madhuri131991 <madhukad13@gmail.com>
2025-03-28 19:50:16 +0530

    adding some contain of git of file adding line2

---
+++ Madhuri/Phython_Programming/Python_variable_folder/python datatype.py
@@ -0,0 +1,5 @@
+#string with number
+var1 = 'hello'
+print(var1*5)
+
+print (""*50)
---
+++ Madhuri/Phython_Programming/Python_variable_folder/subtract integer value.py
@@ -0,0 +1,23 @@
+num1=50
+num2=30
+print("subtraction of num1-num2:",num1-num2)
+
+#multiple of two numbers
+num1=3
+num2=4
+print("multiplication of num1*num2:",num1*num2)
+
+#Program to repeat a given string 5 times.
+str1="SQA"
+n1=5
+print("Result:",str1*n1)
+
+#Python program to get the Average of given numbers
+a=40
+b=50
+c=30
+print("Average of three numbers:",(a+b+c)/3)
+
+#Print the square and cube of a given number.
+n=9
+print("square of number:",n2)
