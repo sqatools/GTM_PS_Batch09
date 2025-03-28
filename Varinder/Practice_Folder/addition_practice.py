a8e4424ce5997dafca0af45c7c2ed5a045ce2bc3
Varinderbatth1 <varinderkbatth@gmail.com>
2025-03-27 22:13:01 -0400

    this is practice file

---
+++ Varinder/Practice_Folder/addition_practice.py
@@ -0,0 +1,26 @@
+nub =20
+numb1 = 140
+numb2 = 30
+total_numb = nub +numb1 + numb2
+print("this is the total =" ,total_numb, type(total_numb))
+
+
+
+num = 20+30+40
+print(num)
+
+
+####################subtractions ##################
+
+num = 100
+num2 = 10
+print( "this is subtraction total :" , num - num2)
+
+
+num = 120
+num1 = 20
+num3 = num - num1
+print(" this is another example of subtraction with total:", num3)
+
+
+
--- Varinder/Practice_Folder/int_practice.py
+++
@@ -1,5 +0,0 @@
-nub =20
-numb1 = 140
-numb2 = 30
-total_numb = nub +numb1 + numb2
-print("this is the total =" ,total_numb, type(total_numb))
