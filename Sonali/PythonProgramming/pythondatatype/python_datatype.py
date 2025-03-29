e69eb2ea609dc0399f01418c9ab19ea20c8bdcdc
sona28392 <sona28392@gmail.com>
2025-03-29 20:18:30 +0530

    datatype

---
+++ Sonali/PythonProgramming/pythondatatype/python_datatype.py
@@ -0,0 +1,40 @@
+print("-"*40)
+# it will print ----------------------------------------
+
+n=4
+print("int value ",n)
+print("-"*40)
+
+n=6.5
+print("float value",n)
+print("-"*40)
+
+
+l1=[2,'d',56.3]
+print("list is:",l1)
+print(type(l1))
+print("-"*40)
+
+
+T1=(2,'d',56.3)
+print("tuple is:",T1)
+print(type(T1))
+print("-"*40)
+
+
+s1={1,'f',5,102,76,358,191873,'j','i','k'}
+print("set value is :",s1)
+print(type(s1))
+print("-"*40)
+
+
+d1={1:"sonali",2:"jhon"}
+print("dict value is :",d1)
+print("-"*40)
+
+list1=[1,3,5,7]
+list2=[2,4,6,8]
+list2.append(list1)
+print(list2)
+print("-"*40)
+
