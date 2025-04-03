e9cb1ad1f244e3971152bb71a33c3948213722b6
Bi12Python <xovek11875@bitvoo.com>
2025-04-03 21:11:24 +0530

    Python Loop Session2

--- Deepesh/PythonProgramming/PythonLoops/Python_loops_concept2.py
+++ Deepesh/PythonProgramming/PythonLoops/Python_loops_concept2.py
@@ -97,3 +97,107 @@ for i in range(1, 6): # i=1, 2, 3, 4
 * *
 *
 """
+
+print("-"*50)
+
+for i in range(6, 0, -1):# i = 6, 5
+    for _ in range(1, i): # (1, 5)
+        print("*", end=" ")
+
+    print()
+
+
+
+#####################################
+# write a python program to print A letter with * pattern.
+
+"""
+  * * *    # i = 1
+*       *  # i = 2
+*       *  # i = 3
+* * * * *  # i = 4
+*       *  # i = 5 
+*       *  # i = 6
+"""
+
+for i in range(1, 7): # i = 1, 2
+    for j in range(1, 6): # j= 1, 2, 3, 4, 5
+        if i == 1:
+            if j == 1 or j == 5:
+                print(" ", end=" ")
+            else:
+                print("*", end=" ")
+        elif i in [2, 3, 5, 6]:
+            if j == 1 or j == 5:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        elif i == 4:
+            print("*", end=" ")
+
+
+    print()
+
+
+print("_"*40)
+########################################
+# write a python program to print O pattern with nested loop
+
+"""
+  * * *    # i = 1
+*       *  # i = 2
+*       *  # i = 3
+*       *  # i = 4
+*       *  # i = 5
+  * * *    # i = 6
+"""
+
+for i in range(1, 7):
+    for j in range(1, 6):
+        if i == 1 or i == 6:
+            if j == 1 or j == 5:
+                print(" ", end=" ")
+            else:
+                print("*", end=" ")
+        elif i in [2, 3, 4, 5]:
+            if j == 1 or j == 5:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+    print()
+
+
+# Home work
+"""
+    *
+  * * *
+* * * * *   
+  * * *
+    *  
+"""
+
+print("_"*50)
+
+"""
+  1 1 1    # i = 1
+2       2  # i = 2
+3       3  # i = 3
+4       4  # i = 4
+5       5  # i = 5
+  6 6 6    # i = 6
+"""
+
+
+for i in range(1, 7):
+    for j in range(1, 6):
+        if i == 1 or i == 6:
+            if j == 1 or j == 5:
+                print(" ", end=" ")
+            else:
+                print(i, end=" ")
+        elif i in [2, 3, 4, 5]:
+            if j == 1 or j == 5:
+                print(i, end=" ")
+            else:
+                print(" ", end=" ")
+    print()
