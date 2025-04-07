aec7cc82166997e58e017888c953d0a9aab266bf
harigit8106 <harishkachu97@gmail.com>
2025-04-07 19:40:47 +0530

    Adding Practiced class of Loops and Patterns

--- Harish/PythonProgramming/Loops/loops.py
+++ Harish/PythonProgramming/Loops/loops.py
@@ -121,6 +121,233 @@ for i in range(1,11,1):
     print()
 print()
 
+for i in range(6,1,-1):
+    for j in range(1,i):
+        print("*", end="")
+    print()
+print()
+
+
+#write a program to print A letter with * pattern
+
+"""
+   * * *
+ *       *
+ *       *
+ * * * * *
+ *       *
+ *       *
+"""
+
+for i in range(1, 7):
+    for j in range(1, 6):
+        if i==1:
+            if j==1 or j==5:
+                print(" ", end="")
+            else:
+                print("*", end="")
+        elif i in [2,3,5,6]:
+            if j==1 or j==5:
+                print("*", end="")
+            else:
+                print(" ", end="")
+        elif i==4:
+                print("*", end="")
+    print()
+
+print("_"*100)
+
+"""
+  * * *    # i = 1
+*       *  # i = 2
+*       *  # i = 3
+*       *  # i = 4
+*       *  # i = 5
+  * * *    # i = 6
+"""
+
+for i in range(1, 7):
+    for j in range(1,6):
+        if i in [1,6]:
+            if j==1 or j==5:
+                print(" ", end="")
+            else:
+                print("*", end="")
+        elif i in [2,3,4,5]:
+            if j==1 or j==5:
+                print("*", end="")
+            else:
+                print(" ", end="")
+
+    print()
+print("_"*100)
+
+# Home work
+"""
+    *
+  * * *
+* * * * *   
+  * * *
+    *  
+"""
+for i in range(1,6):
+    for j in range(1,6):
+        if i==1 or i==5:
+            if j==3:
+                print("*", end="")
+            else:
+                print(" ", end="")
+        elif i in [2,4]:
+            if j==1 or j==5:
+                print(" ", end="")
+            else:
+                print("*", end="")
+        elif i==3:
+            print("*", end="")
+
+    print()
+
+print("_" * 100)
+
+"""
+  1 1 1    # i = 1
+2       2  # i = 2
+3       3  # i = 3
+4       4  # i = 4
+5       5  # i = 5
+  6 6 6    # i = 6
+"""
+
+for i in range(1,7):
+    for j in range(1,6):
+        if i==1 or i==6:
+            if j==1 or j==5:
+                print(" ", end="")
+            else:
+                print(i, end="")
+        elif i in[2,3,4,5]:
+            if j==1 or j==5:
+                print(i, end="")
+            else:
+                print(" ", end="")
+    print()
+
+
+"""
+****
+****
+****
+****
+
+"""
+for i in range(1,5):
+    for j in range(1,5):
+        if i in [1,2, 3, 4]:
+            if j in [1,2, 3, 4]:
+                print("*", end="")
+    print()
+
+print("_" * 100)
+
+"""
+   *
+  **
+ ***
+****
+ ***
+  **
+   *
+"""
+
+
+for i in range(1,8):
+    for j in range(1,5):
+        if i in [1,7]:
+            if j==4:
+                print("*", end="")
+            else:
+                print(" ", end="")
+        elif i in [2,6]:
+            if j in [3,4]:
+                print("*", end="")
+            else:
+                print(" ", end="")
+        elif i in [3,5]:
+            if j in [2,3,4]:
+                print("*", end="")
+            else:
+                print(" ", end="")
+        elif i==4:
+            print("*", end="")
+    print()
+
+print("_" * 100)
+
+"""
+*
+* *
+* * *
+* * * *
+* * * * *
+
+
+for i in range(1,6):
+   for j in range(1,6):
+       if i==1:
+           if j==1:
+               print("*", end="")
+           else:
+               print(" ", end="")
+       elif i == 2:
+           if j in [1,2]:
+               print("*", end="")
+           else:
+               print(" ", end="")
+       elif i == 3:
+           if j in [1, 2, 3]:
+               print("*", end="")
+           else:
+               print(" ", end="")
+       elif i == 4:
+           if j in [1, 2, 3, 4]:
+               print("*", end="")
+           else:
+               print(" ", end="")
+       elif i == 5:
+                print("*", end="")
+print()
+
+"""
+
+"""
+*****
+****
+***
+**
+*
+
+"""
+for i in range(6,1,-1):
+    for j in range(1,i):
+        print("*", end="")
+    print()
+
+
+num=7
+count=0
+for i in range(2, 7):
+    if num%i==0:
+        count+=1
+if count==0:
+    print("PM")
+else:
+    print("NPM")
+
+
+
+
+
+
 
 
 
