9bbe058b628f104f0001f9afb3d6f8a4fe3c875e
Varinderbatth1 <varinderkbatth@gmail.com>
2025-04-14 10:54:03 -0400

    add new files

---
+++ Varinder/python_programing/string_class.py
@@ -0,0 +1,91 @@
+###find string index###
+# name = "Major"
+# print(name, name[3])
+#
+# # apply loop on string
+#
+# name = "python"
+# for i in name:
+#     print(i)
+
+# # apply loop with indexing
+# day = "Monday"
+# str1 = len(day)
+# print(str1*, day)
+# for i in range(str1):
+#     print(i, day[i])
+
+# month = "september"
+# length = len(month)
+# print(length, month)
+# for m in range(length):
+#     print(m, month[m])
+
+##################### String formating ##############
+
+# Name = "Rajan"
+# eys = "Brown"
+# hight1 = 5.7
+#
+# identity ="Name of the person is "+Name+" His eyes are "+eys+" and his hight is "+str(hight1)
+# print(identity)
+
+
+# string = "home"
+# print(string,string[2])
+
+######################
+# program ="Python"
+# for p in program:
+#     print(p)
+#
+############################
+# program = "Python"
+# get_len = len(program)
+# for p in range(get_len):
+#     print(p, program[p])
+##################################
+
+# student = "varinder"
+# program = "Python"
+# class_time = "8:30"
+# no_of_students = 15
+#
+# programing_class = "her name is "+student+" and she is learning "+program+", her class starts at "+class_time+" and total number of students in her class is "+str (no_of_students)
+# print(programing_class)
+#
+# programing_class1 = "her name is {} and she is learning {} her class starts at {} and total numbers of students in her class is {}".format(student,program,class_time,no_of_students)
+# print(programing_class1)
+#
+# programing_class2 = f"her name is {student} and she is learning {program} her class starts at {class_time} and total numbers of students in her class is {no_of_students}"
+# print(programing_class2)
+
+############## string slicing ####################
+# Rule 1: str[start index: end index]
+"""
+->  Output will contains result from left to right.
+->  Output value will include start index and exclude the end index.
+->  if we don't defined the start index, then default start index is 0 e.g. str[:end index]
+->   if we don't defined the end index, then default end index would be end of the string.
+     e.g. str[start index:]
+->
+"""
+name = "Harveer"
+print(name[3:7])# this will print veer
+
+print(name[:-4])# this will print Har
+
+print(name[:2])# this will print Ha
+
+print(name[4:])
+
+print(name[-2:-2])
+
+pr
+
+
+
+
+
+
+
