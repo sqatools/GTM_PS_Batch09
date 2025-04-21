9546a102092ca520c0afe906889ae6a57e53932d
aartip14 <aartip14@gmail.com>
2025-04-21 17:39:16 +0530

    Dictionary Homework Practice

--- Aarti/Python-dictionary/dictionary-programs.py
+++ Aarti/Python-dictionary/dictionary-programs.py
@@ -25,7 +25,7 @@ fruit_price = {'Apple': 50, 'Mango': 30, 'Banana': 20, 'Lichi': 70}
 fruit_purchased = {'Apple': 10, 'Mango': 50, 'Banana': 12, 'Lichi': 100}
 
 total_bill = 0
-
+# Calculate Bill
 print("Fruit Name|Fruit Price|Fruit No|Fruit Bill")
 for fruit, price in fruit_price.items():
     fruit_name = fruit
@@ -39,7 +39,16 @@ for fruit, price in fruit_price.items():
 print("_"*40)
 print("Total bill :", total_bill)
 
+#update inventory
+print("Fruit Inventory : ", fruit_inventory)
+print("Fruit Name|Fruit Purchased|Fruit Inventory")
 
+for fruit, quantity in fruit_purchased.items():
+    fruit_name = fruit
+    fruit_inventory[fruit] = fruit_inventory[fruit] - quantity
+    print(fruit, "\t\t\t", quantity, "\t\t\t", fruit_inventory[fruit])
+
+print("Updated Inventory ", fruit_inventory)
 # Home work:
 # write a python program to provide salary hike to employee on the basis there exp.
 # - if exp is greater 5 and less than equal to 10 :  7%
@@ -51,3 +60,18 @@ employee_list = [
     {'Name': 'Jenny', 'exp': 10, 'age': 45, 'salary': 1000000},
     {'Name': 'rahul', 'exp': 15, 'age': 30, 'salary': 500000}
 ]
+print("Name |\t Exp |\t Age |\t Salary")
+for listitem in employee_list:
+    name = listitem['Name']
+    exp = listitem['exp']
+    age = listitem['age']
+    salary = listitem['salary']
+    increment = 0
+    if(exp > 5 and exp <= 10):
+         increment = (salary * 7/ 100) + salary
+    elif(exp > 10 and exp <= 15):
+        increment = (salary * 10/100) + salary
+    elif(exp <= 5):
+        increment = (salary *20/100) + salary
+
+    print(name, "\t",exp , "\t",age, "\t", salary,"\t", increment)
