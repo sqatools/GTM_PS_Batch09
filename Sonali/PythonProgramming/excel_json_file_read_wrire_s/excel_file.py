f41dabcae23f37bda432de6553eb66de4d88a477
sona28392 <sona28392@gmail.com>
2025-05-03 11:49:05 +0530

    function and variable

--- Sonali/PythonProgramming/excel_json_file_read_wrire_s/excel_file.py
+++ Sonali/PythonProgramming/excel_json_file_read_wrire_s/excel_file.py
@@ -1,6 +1,7 @@
 import openpyxl                        #importing the excel file, so that excel can be used via python
 
 
+
 #print the values from the excel sheet
 
 def excel_data(filepath, sheet_name, cell_name):
@@ -8,7 +9,7 @@ def excel_data(filepath, sheet_name, cell_name):
     sheet9 = wb[sheet_name]
 
     cell = sheet9[cell_name]
-    # print(sheet.cell(i,j).value)
+    print(sheet.cell(i,j).value)
     print(cell.value)
 
 
@@ -24,7 +25,7 @@ for i in range(1, 6):
 
 print("*"*90)
 
-#Write a python to copy the content one column from one sheet to another
+Write a python to copy the content one column from one sheet to another
 
 wb1=openpyxl.load_workbook("python_sonali.xlsx")   #load a excel file
 sheet1=wb1['Sheet4']                #both source sheet and destination sheet define
@@ -44,13 +45,39 @@ wb1.save("python_sonali.xlsx")                       #save the value
 
 print("*"*80)
 
-#write a python program to read first three cell of 5 different sheet.
+write a python program to read first three cell of 5 different sheet.
 
-wb2=openpyxl.load_workbook("python_sonali.xlsx")
-sheets1=[Sheet1,Sheet2,Sheet3,Sheet4,Sheet5]
+def three_cell(filepath,sheet_name,cell_name):
+    wb = openpyxl.load_workbook(filepath)
+    sheet = wb[sheet_name]
+    cell = sheet[cell_name]
+    print(cell.value)
+for i in range(1, 6):
+    print(f"sheet name:Sheet{i}")
+    for j in range(1, 4):
+        three_cell("python_sonali.xlsx", f"Sheet{i}", f"A{j}")
 
-for sheet_name in sheets1:
-    sheet=wb2[sheet_name]
+
+write the python code to read multiple rows and  multiple columns from the sheet
+
+def mult_cell_values(filename,sheet_name,cell_name):
+    wb3 = openpyxl.load_workbook(filename)
+    sheet = wb3[sheet_name]
+    cell = sheet[cell_name]
+    print(cell.value)
+for i in range(1,6):
+    print(f"sheet name:Sheet{i}")
+    for j in ['A','B','C','D']:
+
+        mult_cell_values("python_sonali.xlsx", f"Sheet{i}", f"{j}1")
+        mult_cell_values("python_sonali.xlsx", f"Sheet{i}", f"{j}2")
+        mult_cell_values("python_sonali.xlsx", f"Sheet{i}", f"{j}3")
+
+
+
+
+
+
 
 
 
