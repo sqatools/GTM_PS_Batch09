977b5a714e622b169460cc9be76cdec9e8296dfd
ms123shwe <shwetha131296@gmail.com>
2025-05-21 23:49:30 +0530

    first selenium code

--- Shwetha/SeleniumCode/first_selenium_script.py
+++ Shwetha/SeleniumCode/first_selenium_script.py
@@ -1,20 +1,18 @@
-"""
-pip install selenium
-"""
-import time
 
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.maximize_window()
-driver.implicitly_wait(10)
 driver.get("https://www.facebook.com")
 
 driver.find_element(By.NAME, "email").send_keys("user1@gmail.com")
-driver.find_element(By.NAME, "pass").send_keys("User@12345")
+driver.find_element(By.NAME,"pass").send_keys("User@12345")
 time.sleep(3)
 driver.find_element(By.NAME, "login").click()
-
-time.sleep(10)
+time.sleep(5)
 driver.close()
+
+#########
+
