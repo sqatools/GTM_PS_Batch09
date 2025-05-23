ad30fb2cbccedab90499768ba2486c837ff05810
bhgit25 <bjalvadi@gmail.com>
2025-05-23 21:33:57 +0530

    chnages to push

---
+++ Bharathi/Python Programming/Python OOPS/OOPS.py
@@ -0,0 +1,16 @@
+class school:
+        self.school_name=schoo_name
+
+        def school_name(self, name):
+            print("School name is :", name)
+
+        def school_address(self):
+            print("school address is :", self.school address)
+
+    # create object
+    obj = school('MVM')
+    obj.school_name()
+    obj.school_address()
+
+
+
